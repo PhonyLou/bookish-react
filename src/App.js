6ad271fcf3047c0b90da4c8a8dfd4b64c7a219d7
PhonyLou <louqilin555@gmail.com>
2022-09-19 22:41:25 +0800

    involve material-ui to reduce css file. Using css-in-js approach

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './App.css';
+import {Typography} from "@material-ui/core";
 
 function App() {
-  return (
-      <div className='App'>
-        <h1>Hello World</h1>
-      </div>
-  );
+    return (
+        <div className='App'>
+            <Typography variant='h2' component='h2' data-test='heading'>
+                Bookish
+            </Typography>
+        </div>
+    );
 }
 
 export default App;
